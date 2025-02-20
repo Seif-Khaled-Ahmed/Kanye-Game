c43bea07d4e14a636e70115db2a2ca69d118f426
Seif-Khaled-Ahmed <seif.khaled.17@gmail.com>
2025-02-20 19:49:00 +0200

    first commit

---
+++ front-end/src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+import image1 from "./images/output_35.png";
+import Counter from "./Animations/Counter/Counter.jsx";
+
+function App() {
+  return (
+    <div id="big-big-container">
+      <div id="left-container">
+        <div id="container">
+          <div id="image">
+            <img src={image1} alt="Example "></img>
+          </div>
+        </div>
+      </div>
+      <div id="right-container">
+        <div id="score">
+          <p>Score</p>
+          <Counter
+            value={100}
+            places={[100, 10, 1]}
+            fontSize={80}
+            padding={5}
+            gap={10}
+            textColor="white"
+            fontWeight={900}
+          />
+        </div>
+        <div id="choices">
+          <button className="choice">HI</button>
+          <button className="choice">NIGGERS</button>
+          <button className="choice">Click Me</button>
+          <button className="choice">Click Me</button>
+          <button className="choice">NIGGERS</button>
+          <button className="choice">Click Me</button>
+          <button className="choice">Click Me</button>
+          <button className="choice">Click Me</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
