460da0c9441a98d81988e22652ff147b3ab5a3bc
Seif-Khaled-Ahmed <seif.khaled.17@gmail.com>
2025-02-26 00:11:08 +0200

    leaderboard and score api

--- back-end/src/app-controller.py
+++ back-end/src/app-controller.py
@@ -25,7 +25,8 @@ app.config['SECRET_KEY'] = 'string1'
 def set_session():
     session["id"] = str(int(time.time())) +"howa_keda"+ str(random.randint(0,2147483647))
     print(session.get("id"))
-    return ("",204)
+    leaderboard.insert_one({'id':session.get('id'),'name':None,'score':0})
+    return ("", 204)
 
 
 @app.route('/image', methods=['GET']) 
@@ -33,32 +34,93 @@ def get_image():
     if (not 'id' in session):
         set_session()
     img_id = random.randint(0, png_count)
-    words = hide_random_word(f"../media/{img_id}.png", output_path=f"output/output_{img_id}.png")
-    print(img_id)
-    print (words)
-    fiveara = {'id':str (session.get('id')) ,'words':words}
-    answers.insert_one(fiveara)
+    try:
+        words = hide_random_word(f"../media/{img_id}.png", output_path=f"output/output_{img_id}.png")
+    except:
+        return("internal server error", 500)
+    print("image number:", img_id)
+    print(words)
+    answer = {'id':str (session.get('id')), 'words':words}
+    answers.insert_one(answer)
     return(send_file(f'output/output_{img_id}.png'))
 
 
 @app.route('/random_answers', methods=['GET']) 
 def get_answers():
-    notrandom = answers.find_one({'id': str(session.get('id'))})['words']
+    try:
+        notrandom = answers.find_one({'id': str(session.get('id'))})['words']
+    except:
+        return("user not found", 404)
     if len(notrandom) == 1:
         return notrandom
-    randomized ={}
+    randomized = {}
     sample  = random.sample(notrandom.keys(),len(notrandom))
     for i in range(len(notrandom)):
         randomized[i] = notrandom[sample[i]]
-
     return (randomized)
 
-@app.route('/answer', methods=['GET']) 
-def get_answer():
-    data = request.get_json()
-    return({})
+@app.route('/answer/<number>/<word>', methods=['PUT']) 
+def get_answer(number,word):
+    number = int(number)
+    try:
+        sid = str(session.get('id'))
+    except Exception as e:
+        return(f"session id error {e}",400)
+    try:
+        round = answers.find_one({'id':sid})
+    except Exception as e:
+        return (f"round not found {e}", 404)
+    score = leaderboard.find_one({'id':sid})
+    print(score['score'])
+    try:
+        if(number > len(round['words'])):
+            return("wrong input", 400)
+        if(round['words'][str(number)] == word):
+            newvalue = {"$set": { "score": score['score'] + 1 }}
+        else :
+            newvalue = {"$set": { "score": score['score'] - 1 }}
+    except Exception as e:
+        return(f"server error {e}", 500)
+    leaderboard.update_one(score,newvalue)
+    score = leaderboard.find_one({'id':sid})
+    return(str(score))
+
+@app.route('/score', methods=['GET'])
+def get_score():
+    sid = str(session.get('id'))
+    try:
+        user = answers.find_one({'id':sid})
+    except:
+        return ("user not found", 404)
+    try:
+        score = leaderboard.find_one({'id':sid})['score']
+    except:
+        return("score not found", 404)
+    return(str(score))
 
 
+@app.route('/leaderboard', methods=['GET'])
+def get_leaderboard():
+    try:
+        scores = []
+        scoreall = leaderboard.find().sort("score")
+        num = min(leaderboard.count_documents({}),50)
+        for x in range(num):
+            scores.append(scoreall[x])
+
+    except:
+        return("server error", 500)
+    
+    return(str(scores))
+
+
+@app.route('/name', methods=['POST'])
+def set_username():
+    return(0)
+
+@app.route('/submit',methods=['DELETE'])
+def submit():
+    return(0)
 
 
 if __name__ == '__main__':
